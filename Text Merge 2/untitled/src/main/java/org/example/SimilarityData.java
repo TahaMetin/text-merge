0861ec9c19f6d8afd63161353d20618b18e9ce39
Taha <55713590+TahaMetin@users.noreply.github.com>
2023-03-28 18:23:12 +0300

    Revert "Project merged with frontend locally and updated github"
    This reverts commit 50b2a116d4a92d8f757ae11f1fd38d09f065e4fa.

---
+++ Text Merge 2/untitled/src/main/java/org/example/SimilarityData.java
@@ -0,0 +1,43 @@
+package org.example;
+
+import javax.swing.plaf.basic.BasicComboBoxUI;
+
+public class SimilarityData {
+    private int textNo1;
+    private int textNo2;
+    private Double similarityScore;
+
+    public SimilarityData(int textNo1, int textNo2, double similarityScore) {
+        this.textNo1 = textNo1;
+        this.textNo2 = textNo2;
+        this.similarityScore = similarityScore;
+    }
+    public SimilarityData() {
+        this.textNo1 = 0;
+        this.textNo2  =0;
+        this.similarityScore = 0.0;
+    }
+    public int getTextNo1() {
+        return textNo1;
+    }
+
+    public void setTextNo1(int textNo1) {
+        this.textNo1 = textNo1;
+    }
+
+    public int getTextNo2() {
+        return textNo2;
+    }
+
+    public void setTextNo2(int textNo2) {
+        this.textNo2 = textNo2;
+    }
+
+    public double getSimilarityScore() {
+        return similarityScore;
+    }
+
+    public void setSimilarityScore(double similarityScore) {
+        this.similarityScore = similarityScore;
+    }
+}
